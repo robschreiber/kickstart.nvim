ad6f79e248977ddf3c435643058b54ade9bfae84
robschreiber <rob.schreiber@yardi.com>
2024-04-09 10:04:58 -0600

    Add git description to which

--- init.lua
+++ init.lua
@@ -295,6 +295,7 @@ require('lazy').setup({
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
       }
     end,
   },
