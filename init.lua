b3bccc8980fb6fffbaa8d9b5f9c01e887b31f0bb
Rob Schreiber <rob.schreiber@yardi.com>
2024-04-23 11:43:45 -0600

    Add arrow keymap description and remove harpoon

--- init.lua
+++ init.lua
@@ -324,8 +324,9 @@ require('lazy').setup({
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
         ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        [';'] = { 'Arrow Files' },
+        ['m'] = { 'Arrow Buffer' },
       }
     end,
   },
