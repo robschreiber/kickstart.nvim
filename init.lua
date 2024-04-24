094ebdbe633a0dea4e154621f5b3a4229915d8bd
Rob Schreiber <rob.schreiber@yardi.com>
2024-04-24 16:43:28 -0600

    Revert picker change

--- init.lua
+++ init.lua
@@ -393,11 +393,6 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        pickers = {
-          find_files = {
-            hidden = true,
-          },
-        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
